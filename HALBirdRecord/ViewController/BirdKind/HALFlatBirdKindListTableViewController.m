5a3d72d5df8f64514aee43f864e3113649aa140f
halhorn <halhorn@halmidi.com>
2013-12-08 16:50:02 +0900

    show section separator

--- HALBirdRecord/ViewController/BirdKind/HALFlatBirdKindListTableViewController.m
+++ HALBirdRecord/ViewController/BirdKind/HALFlatBirdKindListTableViewController.m
@@ -85,6 +85,13 @@
     return cell;
 }
 
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSArray *birdGroup = self.birdKind.birdKindList[section];
+    HALBirdKindEntity * entity = birdGroup[0];
+    return entity.groupName;
+}
+
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
