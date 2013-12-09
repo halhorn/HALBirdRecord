ac951c4a32b513b605111bdef4a036a651d66dd3
halhorn <halhorn@halmidi.com>
2013-12-09 23:38:27 +0900

    add activity entity

---
+++ HALBirdRecord/Entity/HALActivityRecordEntity.h
@@ -0,0 +1,19 @@
+//
+//  HALActivityRecordEntity.h
+//  HALBirdRecord
+//
+//  Created by 信田 春満 on 2013/12/09.
+//  Copyright (c) 2013年 halhorn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HALActivityRecordEntity : NSObject
+
+@property(nonatomic) int dbID;
+@property(nonatomic) NSString *title;
+@property(nonatomic) NSString *location;
+@property(nonatomic) NSString *comment;
+@property(nonatomic) NSDate *datetime;
+
+@end
---
+++ HALBirdRecord/Entity/HALActivityRecordEntity.m
@@ -0,0 +1,13 @@
+//
+//  HALActivityRecordEntity.m
+//  HALBirdRecord
+//
+//  Created by 信田 春満 on 2013/12/09.
+//  Copyright (c) 2013年 halhorn. All rights reserved.
+//
+
+#import "HALActivityRecordEntity.h"
+
+@implementation HALActivityRecordEntity
+
+@end
