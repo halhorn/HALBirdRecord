805ece3d3e081434a749e6c6ee59e3ff9fb6de9c
halhorn <halhorn@halmidi.com>
2013-12-11 21:44:49 +0900

    add views

---
+++ HALBirdRecord/ViewController/RecordList/HALRecordListViewController.m
@@ -0,0 +1,38 @@
+//
+//  HALRecordListViewController.m
+//  HALBirdRecord
+//
+//  Created by 信田 春満 on 2013/12/11.
+//  Copyright (c) 2013年 halhorn. All rights reserved.
+//
+
+#import "HALRecordListViewController.h"
+
+@interface HALRecordListViewController ()
+
+@end
+
+@implementation HALRecordListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
