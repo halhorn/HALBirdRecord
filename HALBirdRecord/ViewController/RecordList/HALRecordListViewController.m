21b543a84b60d77105ee81011b3132c071fa22b6
halhorn <halhorn@halmidi.com>
2013-12-12 22:38:46 +0900

    add new activity button

--- HALBirdRecord/ViewController/RecordList/HALRecordListViewController.m
+++ HALBirdRecord/ViewController/RecordList/HALRecordListViewController.m
@@ -8,6 +8,7 @@
 
 #import "HALRecordListViewController.h"
 #import "HALRecordListTableViewController.h"
+#import "HALBirdKindListViewController.h"
 #import "UIViewController+HALViewControllerFromNib.h"
 
 @interface HALRecordListViewController ()
@@ -33,12 +34,24 @@
     // Do any additional setup after loading the view from its nib.
     self.activityRecordTableViewController = [HALRecordListTableViewController viewControllerFromNib];
     [self.activityRecordView addSubview:self.activityRecordTableViewController.view];
+    [self setupNavBar];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setupNavBar
+{
+    WeakSelf weakSelf = self;
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"＋" style:UIBarButtonItemStyleBordered handler:^(id sender){
+        HALBirdKindListViewController *viewController = [HALBirdKindListViewController viewControllerFromNib];
+        UINavigationController *navBarController = [[UINavigationController alloc] initWithRootViewController:viewController];
+        navBarController.navigationBar.translucent = NO;
+        [weakSelf presentViewController:navBarController animated:YES completion:nil];
+    }];
 }
 
 @end
