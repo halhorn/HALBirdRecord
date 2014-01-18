62db0de544f2ce71e261067ea176b425f722411c
halhorn <halhorn@halmidi.com>
2014-01-18 11:07:50 +0900

    fix design

--- HALBirdRecord/ViewController/Activity/HALBirdRecordTableViewCell.m
+++ HALBirdRecord/ViewController/Activity/HALBirdRecordTableViewCell.m
@@ -46,7 +46,7 @@ static NSDateFormatter *dateFormatter;
 {
     if (!dateFormatter) {
         dateFormatter = [[NSDateFormatter alloc] init];
-        dateFormatter.dateFormat = @"HH:mm:ss";
+        dateFormatter.dateFormat = @"MM/dd HH:mm:ss";
     }
     self.birdImageView.image = birdRecord.kind.image;
     self.birdKindLabel.text = birdRecord.kind.name;
