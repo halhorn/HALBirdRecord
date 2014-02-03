ffec11480ffb95eaad5a7c9b251583d67e666123
halhorn <halhorn@halmidi.com>
2014-02-03 22:39:33 +0900

    implement notify_student_authentication_request

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -1,6 +1,31 @@
+var secret = require('cloud/secret.js');
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+var Mandrill = require('mandrill');
+Mandrill.initialize(secret.get_mandorill_api_key());
+
+Parse.Cloud.define("notify_student_authentication_request", function(request, response) {
+  Mandrill.sendEmail({
+    message: {
+      from_email: "halhorn@halidi.com",
+      from_name: "Bird Record",
+      to: [
+        {
+          email: "halhorn@halmidi.com",
+          name: "HAL"
+        }
+      ],
+      subject: "HALBirdRecord - Student Authenticate Request",
+      text: "You get student authentication request.\n\nhttps://parse.com/apps/halbirdrecord/collections"
+    },
+    async: true
+  },{
+    success: function(httpResponse) {
+      console.log(httpResponse);
+      response.success("Email sent!");
+    },
+    error: function(httpResponse) {
+      console.error(httpResponse);
+      response.error("Uh oh, something went wrong");
+    }
+  });
 });
